b563d37663aad505178e417307b4d28e9b76402c
ezratweaver <ezratweaver@gmail.com>
2023-07-13 17:49:17 -0500

    Added New Functions

--- database_manager.py
+++ database_manager.py
@@ -6,3 +6,21 @@ cursor = db_connection.cursor()
 
 cursor.execute("SELECT * FROM articles")
 
+known_articles = cursor.fetchall()
+
+
+def check_for_article(article_tuple):
+    for article in known_articles:
+        if article_tuple == article:
+            return True
+    return False
+
+def add_entry(title, date, author, source, description, url, image):
+    cursor.execute(f"""INSERT INTO articles
+        (article_title, published_date, author, source, 
+        article_description, article_url, cover_image)
+        VALUES ('{title}', '{date}', '{author}', '{source}', 
+        '{description}', '{url}', '{image}')""")
+    db_connection.commit()
+    db_connection.close()
+
