2d70609a79fc14a33b5adfbee425a2ee66308d7a
ezratweaver <ezratweaver@gmail.com>
2023-07-13 17:49:28 -0500

    sort_article_data Tweaks

--- search.py
+++ search.py
@@ -2,6 +2,7 @@ import requests
 import os
 from datetime import date
 from dotenv import load_dotenv
+from database_manager import add_entry, check_for_article
 
 load_dotenv()
 api_key = os.environ.get("api")
@@ -9,6 +10,7 @@ api_key = os.environ.get("api")
 
 PROMPT = "Red Wing Scarlet Kitchen and Bar"
 FROM_DATE = str(date.today())
+FROM_DATE = "2023-06-13"
 
 def find_articles(prompt, from_date, api_key) -> dict:
     query = f"https://newsapi.org/v2/everything?q={prompt}&from={from_date}&sortBy=publishedAt&apiKey={api_key}"
@@ -23,23 +25,22 @@ def find_articles(prompt, from_date, api_key) -> dict:
 
 def sort_article_data(articles):
     for article in articles:
-        article_title = article["title"]
-        published_date = article["publishedAt"]
-        author = article["author"]
-        source = article["source"]["name"]
-        article_description = article["description"]
-        article_url = article["url"]
-        cover_image = article["urlToImage"]
+        article_data = (article["title"], article["publishedAt"], 
+                        article["author"], article["source"]["name"], 
+                        article["description"], article["url"],
+                        article["urlToImage"])
         
+        if check_for_article(article_data):
+            continue
 
         print("--------------------")
-        print("Article Title:", article_title)
-        print("Published Date:", published_date)
-        print("Author:", author)
-        print("Source Name:", source)
-        print("Article Description:", article_description)
-        print("Article URL:", article_url)
-        print("Cover Image:", cover_image)
+        print("Article Title:", article_data[0])
+        print("Published Date:", article_data[1])
+        print("Author:", article_data[2])
+        print("Source Name:", article_data[3])
+        print("Article Description:", article_data[4])
+        print("Article URL:", article_data[5])
+        print("Cover Image:", article_data[6])
 
 
 total_results, articles = find_articles(PROMPT, FROM_DATE, api_key)
